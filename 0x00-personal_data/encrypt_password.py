6979d0f64315c5778c3f8add9c62bf1878ec3499
Ikenna <ikenna@kali.Ikenna-PC>
2023-04-30 01:20:34 +0100

    (fixed) Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,7 +5,7 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """
      function that expects one string argument name password
      and returns a salted, hashed password, which is a byte string
