47dcea22f29d385a3a73be39d9a60e9a04a4e363
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-03 13:46:56 +0100

    unauthorized error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'],
+                 strict_slashes=False)
+def error_test():
+  """
+  testing 401 error handler
+  """
+  abort(401)
+
