8ecf8d678d9d824082bce6dd472d79e38ddc1847
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-03 18:49:02 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Basic auth"""
+import base64
+import binascii
 
 
 class BasicAuth:
@@ -17,3 +19,21 @@ class BasicAuth:
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """
+         returns the decoded value of a Base64 string
+         base64_authorization_header
+        """
+        if type(base64_authorization_header) is not str or\
+                base64_authorization_header is None:
+            return None
+        try:
+            base64_bytes = base64_authorization_header.encode('ascii')
+            decode_bytes = base64.b64decode(base64_bytes)
+            decode_str = decode_bytes.decode('utf-8')
+            return decode_str
+        except (binascii.Error, UnicodeDecodeError):
+            return None
