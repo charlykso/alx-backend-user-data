464a7a8409463b7feb7005217f477abc6debd8cb
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-12 11:40:49 +0100

    Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,4 +90,15 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
         return None
 
-    
+    def get_reset_password_token(self, email: str) -> str:
+        """get token password reset
+        :param email: user email
+        :return: string
+        """
+        try:
+            user = self._db.find_user_by(email)
+        except (NoResultFound, InvalidRequestError):
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
