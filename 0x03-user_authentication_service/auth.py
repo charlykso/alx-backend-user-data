b769654870a161c012227620190f75a298b59720
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-12 10:08:34 +0100

    (fixed 1.0) Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,10 +53,10 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'),
-                                  user.hashed_password)
         except NoResultFound:
             return False
+        return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
 
     def create_session(self, email: str) -> str:
         """create a sessionId and store it in the db
