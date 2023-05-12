6c2ae36061b444a5511dda473aaf9723bf4f9465
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-12 11:29:37 +0100

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,7 +34,23 @@ def register_user() -> Tuple[str, int]:
         return jsonify(response_msg), 400
 
 
-@app.route('/session', methods=['DELETE'], strict_slashes=False)
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """implements login"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_login = AUTH.valid_login(email, password)
+
+    if not valid_login:
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({'email': email, 'message': 'logged in'})
+    response.set_cookie('session_id', session_id)
+    return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout() -> Union[abort, redirect]:
     """logout user"""
     session_id = request.cookies.get('session_id')
@@ -42,7 +58,18 @@ def logout() -> Union[abort, redirect]:
     if session_id is None or user is None:
         abort(403)
     AUTH.destroy_session(user.id)
-    return redirect('/')  
+    return redirect('/')
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """user profile"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if session_id is None or user is None:
+        abort(403)
+    return jsonify({"email": user.email})
 
 
 if __name__ == "__main__":
