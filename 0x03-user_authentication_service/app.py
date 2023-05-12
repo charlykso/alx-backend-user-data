da3bccacdd472c1550397523e2860fd930d9d12f
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-12 10:46:14 +0100

    Find user by session ID

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, jsonify, request
 from auth import Auth
+from typing import Tuple, Union
 
 
 app = Flask(__name__)
@@ -10,13 +11,13 @@ AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def home():
+def index() -> str:
     """the home route"""
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def register():
+def register_user() -> Tuple[str, int]:
     """register user"""
     email = request.form.get('email')
     password = request.form.get('password')
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,11 +2,12 @@
 """Auth file"""
 
 import uuid
-import bcrypt
+from bcrypt import hashpw, checkpw, gensalt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -15,7 +16,7 @@ def _hash_password(password: str) -> bytes:
     :param password: a string password to be hashed
     :return: bytes of the hashed password
     """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashpw(password.encode('utf-8'), gensalt())
 
 
 def _generate_uuid() -> str:
@@ -58,9 +59,9 @@ class Auth:
             return False
         hashed_password = user.hashed_password.encode('utf-8')
 
-        return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+        return checkpw(password.encode('utf-8'), hashed_password)
 
-    def create_session(self, email: str) -> str:
+    def create_session(self, email: str) -> Union[str, None]:
         """create a sessionId and store it in the db
         :param email: email of the user for the session id
         :return: the sessionId created
@@ -72,3 +73,14 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user_id=found_user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """
+        get user using the session id
+        :param session_id: the session_id of the user
+        :return: coresponding user with the session_id
+        """
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
