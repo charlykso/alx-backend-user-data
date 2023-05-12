106cd8eac11a4f55e31dd1f8eb73a0a88781d304
Ikenna <ikenna@kali.Ikenna-PC>
2023-05-12 12:07:24 +0100

    Update password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -72,5 +72,17 @@ def profile() -> str:
     return jsonify({"email": user.email})
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> Union[str, abort]:
+    """get reset password token"""
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email=email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "reset_token": reset_token})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
